b858c79979a971fd04e0f38141933c60f44d1761
Thomas <2985511+teakimm@users.noreply.github.com>
2023-04-04 04:11:26 -0500

    Create initial render and home module

--- main.js
+++ main.js
@@ -1,1 +1,106 @@
-(()=>{"use strict";!function(){const t=document.getElementById("content"),e=document.createElement("div");e.className="title",e.textContent="yeh",t.appendChild(e)}()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction createTitle(name, text) {\n  const title = document.createElement(\"h1\");\n  title.setAttribute(\"class\", name);\n  title.textContent = text;\n  return title;\n}\nfunction renderHome() {\n  const main = document.querySelector(\"main\");\n  const title = createTitle(\"title\", \"Aiya Ramen\");\n  main.appendChild(title);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (renderHome);\n\n//# sourceURL=webpack://restaurant-page/./src/home.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _initialRender__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./initialRender */ \"./src/initialRender.js\");\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n\n\n(0,_initialRender__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n(0,_home__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/initialRender.js":
+/*!******************************!*\
+  !*** ./src/initialRender.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction createBtn(name, text) {\n  const btn = document.createElement(\"button\");\n  btn.setAttribute(\"class\", name);\n  btn.textContent = text;\n  return btn;\n}\nfunction createNav(name) {\n  const nav = document.createElement(\"nav\");\n  nav.setAttribute(\"class\", name);\n  const homeBtn = createBtn(\"homeBtn\", \"Home\");\n  const menuBtn = createBtn(\"menuBtn\", \"Menu\");\n  const aboutBtn = createBtn(\"aboutBtn\", \"About\");\n  const contactBtn = createBtn(\"contactBtn\", \"Contact\");\n  nav.appendChild(homeBtn);\n  nav.appendChild(menuBtn);\n  nav.appendChild(aboutBtn);\n  nav.appendChild(contactBtn);\n  return nav;\n}\nfunction createMain(name) {\n  const main = document.createElement(\"main\");\n  main.setAttribute(\"class\", name);\n  return main;\n}\nfunction createFooter(name) {\n  const footer = document.createElement(\"footer\");\n  footer.setAttribute(\"class\", name);\n  const reference = document.createElement(\"div\");\n  const background = document.createElement(\"div\");\n  reference.setAttribute(\"class\", \"reference\");\n  reference.textContent = \"Referred to link here for design\";\n  background.setAttribute(\"class\", \"background\");\n  background.textContent = \"Background images by link here\"\n  footer.appendChild(reference);\n  footer.appendChild(background);\n  return footer;\n}\nfunction initialRender() {\n  const content = document.getElementById(\"content\");\n  const nav = createNav(\"nav-bar\");\n  const main = createMain(\"main\");\n  const footer = createFooter(\"footer\");\n  content.appendChild(nav);\n  content.appendChild(main);\n  content.appendChild(footer);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (initialRender);\n\n//# sourceURL=webpack://restaurant-page/./src/initialRender.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
